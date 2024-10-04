6d3d3ce8b145e563d3d97f4209f855c608022654
MireX420 <adrian_mirek@yahoo.no>
2024-10-04 12:03:58 +0200

    ye

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./css/style.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 import Home from './components/Home';
 import Neurolink from './components/Neurolink';
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/neurolink" element={<Neurolink />} />
+        <Route path="/header" element={<Header />} />
       </Routes>
     </Router>
   );
